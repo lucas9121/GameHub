9ad472423e0eb4e3680a9fbf1d9af4e866e55715
Lucas <lucas2carlos@gmail.com>
2022-04-20 21:14:03 -0400

    building routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css'
 import {Routes, Route} from "react-router-dom"
+import Home from './pages/Home'
 
 export default function App(){
     return(
         <main className='App'>
             <Routes>
-                <Route />
+                <Route path='/' element={<Home />} />
             </Routes>
         </main>
     )
