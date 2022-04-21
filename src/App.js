837e54e3f72bc4a8c3f421f97bbac1b6ee01995d
Lucas <lucas2carlos@gmail.com>
2022-04-20 21:21:01 -0400

    building routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import {Routes, Route} from "react-router-dom"
 import Home from './pages/Home'
 import MyAccount from './pages/MyAccount'
+import Cart from './pages/Cart'
 
 export default function App(){
     return(
@@ -9,6 +10,7 @@ export default function App(){
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/account' element={<MyAccount />} />
+                <Route path='/cart' element={<Cart />} />
             </Routes>
         </main>
     )
