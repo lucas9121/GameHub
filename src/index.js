53bdeb8cdf0d02fc15ded223288dbcfb1a7bc416
Lucas <lucas2carlos@gmail.com>
2022-04-20 20:14:16 -0400

    building boilerplate

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import {React, StrictMode} from "react";
-import { ReactDOM } from "react-dom";
+import ReactDOM  from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
