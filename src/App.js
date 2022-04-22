04b508da4dd247ad6e54430f86c16adb4b9b294d
Lucas <lucas2carlos@gmail.com>
2022-04-21 20:51:39 -0400

    adding games api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css'
 import {Routes, Route} from "react-router-dom"
+import { useState, useEffect } from "react"
 import Home from './pages/Home'
 import MyAccount from './pages/MyAccount'
 import Cart from './pages/Cart'
 
 export default function App(){
+    const [games, setGames] = useState([])
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const res = await fetch('http://localhost:3001/api/games')
+                const data = await res.json()
+                setGames(data)
+            } catch (e) {
+                console.log(e)
+            }
+        })()
+    }, [])
+
     return(
         <main className='App'>
             <Routes>
-                <Route path='/' element={<Home />} />
+                <Route path='/' element={<Home games={games}/>} />
                 <Route path='/account' element={<MyAccount />} />
                 <Route path='/cart' element={<Cart />} />
             </Routes>
