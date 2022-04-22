47fdf370e179c9af682a96390ce350249cdd23c5
Lucas <lucas2carlos@gmail.com>
2022-04-21 20:23:47 -0400

    building routes

--- routes/api/games.js
+++ routes/api/games.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const gamesCtrl = 
+const gamesCtrl = require('../../controllers/api/games')
 
 
+
+// GET /api/games/seed
+router.get('/seed', gamesCtrl.seed)
 
 // GET /api/games/index
 router.get('/', gamesCtrl.index)
