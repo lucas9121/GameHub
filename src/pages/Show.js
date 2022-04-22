5ca4e0712b1486df24944e73f5938f72733c5863
Lucas <lucas2carlos@gmail.com>
2022-04-21 22:44:58 -0400

    building show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,49 @@
+import { useState, useEffect } from "react"
+import { useParams, Link } from "react-router-dom"
+// import game from "../../models/game"
+
 export default function Show() {
+    const {id} = useParams()
+    const [game, setGame] = useState({})
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await fetch(`http://localhost:3001/api/games/${id}`)
+                const data = response.json()
+                console.log(data)
+                console.log(data.promiseResult)
+                console.log(response)
+                console.log(response.data)
+                setGame(data)
+            } catch(e) {
+                console.log('Error!!!!!!!!!!!!!')
+                console.log(e)
+            }
+        })() 
+    }, [])
+
     return(
-        <h1>Show page</h1>
+        <main className="show-div">
+            <h2>{game.name} </h2>
+            <img src={game.img} alt={game.name} />
+            <div className="purchase-div">
+                <div className="purchase-title">
+                    <h4>Buy {game.name} </h4>
+                </div>
+                <p>Quantity: {game.qty} </p>
+                <div>
+                    <p>Price: {game.price}</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+            <div className="about-div">
+                <h3>About this Game:</h3>
+                <hr />
+                <div className="about-description">
+                    <p>{game.description} </p>
+                </div>
+            </div>
+        </main>
     )
 }
