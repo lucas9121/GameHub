9c36a31de645ac2971c6a332418871db8c245180
Lucas <lucas2carlos@gmail.com>
2022-04-25 17:59:06 -0400

    building edit page

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -20,32 +20,32 @@ export default function Edit() {
     return (
         <main className="Edit">
             <h2>Edit {game.name} </h2>
-            <form className='edit-form' action={`/dev/${this.props.game._id}?_method=PUT`} method="POST">
+            <form className='edit-form' action={`/api/games/${game._id}?_method=PUT`} method="POST">
                 <div className='row'>
                     <div className='form-group col'>
                         <label htmlFor='name' className='text-primary'>Name</label>
-                        <input name="name" type="text" defaultValue={this.props.game.name} className='form-control form-control-sm' id='name'/>
+                        <input name="name" type="text" defaultValue={game.name} className='form-control form-control-sm' id='name'/>
                     </div>
                     <div className='form-group col'>
                         <label htmlFor="price" className='text-primary'>Price</label>
-                        <input name="price" type="number" defaultValue={this.props.game.price} className='form-control form-control-sm' id='price'/>
+                        <input name="price" type="number" defaultValue={game.price} className='form-control form-control-sm' id='price'/>
                     </div>
                     <div className='form-group col'>
                         <label htmlFor="qty" className='text-primary'>Quantity</label>
-                        <input name="qty" type="number" defaultValue={this.props.game.qty} className='form-control form-control-sm' id='qty'/>
+                        <input name="qty" type="number" defaultValue={game.qty} className='form-control form-control-sm' id='qty'/>
                     </div>
                     <div className='form-group col'>
                         <label htmlFor="img" className='text-primary'>Image</label>
-                        <input name="img" type="url" defaultValue={this.props.game.img} className='form-control form-control-sm' id='url'/>
+                        <input name="img" type="url" defaultValue={game.img} className='form-control form-control-sm' id='url'/>
                     </div>
                 </div>
                 <div className='form-group'>
                     <label htmlFor="description" className='text-primary'>  Description</label>
-                    <textarea name="description" defaultValue={this.props.game.description} id="description description-box" maxLength={'500'} className='form-control' cols="40" rows="3"></textarea>
+                    <textarea name="description" defaultValue={game.description} id="description description-box" maxLength={'500'} className='form-control' cols="40" rows="3"></textarea>
                 </div>
                 <input className='btn btn-outline-success' type="submit" value="Edit Game" />
             </form>
-            <form action={`/dev/${this.props.game._id}?_method=DELETE`} method="POST">
+            <form action={`/dev/${game._id}?_method=DELETE`} method="POST">
                 <input className='btn btn-danger' type="submit" value='Delete Game' />
             </form>
         </main>
