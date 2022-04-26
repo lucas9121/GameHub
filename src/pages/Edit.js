509d5dd35723181670458c67a3cbd6ef83aa5d74
Lucas <lucas2carlos@gmail.com>
2022-04-25 21:44:15 -0400

    building edit page

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import { useParams, Link } from "react-router-dom"
 // import axios from "axios"
 
@@ -6,7 +6,11 @@ import { useParams, Link } from "react-router-dom"
 export default function Edit() {
     const {id} = useParams()
     const [game, setGame] = useState({})
-    const [newData, setNewData] = useState({})
+    const name = useRef(null)
+    const price = useRef(null)
+    const qty = useRef(null)
+    const img = useRef(null)
+    const description = useRef(null)
 
     // useEffect(() => {
     //     (async () => {
@@ -20,9 +24,11 @@ export default function Edit() {
     //     })() 
     // }, [])
 
-    const handleChange = (event) => {
-        setNewData({[event.target.name]: event.target.value})
-    }
+    // name: 'Game 1',
+    // price: price.current.value,
+    // qty: qty.current.value,
+    // img: img.current.value,
+    // description: description.current.value
 
     const handleSubmit = async (event) => {
         event.preventDefault()
@@ -30,7 +36,7 @@ export default function Edit() {
             const response = await fetch(`http://localhost:3001/api/games/${id}`, {
                 method: 'PUT',
                 body: {
-                    name: 'New Game 1 name'
+                    name: 'Game 1 new name'
                 }
             })
         } catch(e) {
@@ -41,31 +47,31 @@ export default function Edit() {
     return (
         <main className="Edit">
             <h2>Edit {game.name} </h2>
-            <Link to={`/${game._id}`} >Back</Link>
+            <Link to={`/${id}`} >Back</Link>
             <br />
             <br />
             <form className='edit-form' onSubmit={handleSubmit}  method="POST">
                 <div className='row'>
                     <div className='form-group col'>
                         <label htmlFor='name' className='text-primary'>Name</label>
-                        <input name="name" type="text" onChange={handleChange} defaultValue={game.name} className='form-control form-control-sm' id='name'/>
+                        <input name="name" type="text" ref={name} defaultValue={game.name} className='form-control form-control-sm' id='name'/>
                     </div>
                     <div className='form-group col'>
                         <label htmlFor="price" className='text-primary'>Price</label>
-                        <input name="price" type="number" defaultValue={game.price} className='form-control form-control-sm' id='price'/>
+                        <input name="price" type="number" ref={price} defaultValue={game.price} className='form-control form-control-sm' id='price'/>
                     </div>
                     <div className='form-group col'>
                         <label htmlFor="qty" className='text-primary'>Quantity</label>
-                        <input name="qty" type="number" onChange={handleChange} defaultValue={game.qty} className='form-control form-control-sm' id='qty'/>
+                        <input name="qty" type="number" ref={qty} defaultValue={game.qty} className='form-control form-control-sm' id='qty'/>
                     </div>
                     <div className='form-group col'>
                         <label htmlFor="img" className='text-primary'>Image</label>
-                        <input name="img" type="url" onChange={handleChange} defaultValue={game.img} className='form-control form-control-sm' id='url'/>
+                        <input name="img" type="url" ref={img} defaultValue={game.img} className='form-control form-control-sm' id='url'/>
                     </div>
                 </div>
                 <div className='form-group'>
                     <label htmlFor="description" className='text-primary'>  Description</label>
-                    <textarea name="description" onChange={handleChange} defaultValue={game.description} id="description description-box" maxLength={'500'} className='form-control' cols="40" rows="3"></textarea>
+                    <textarea name="description" ref={description} defaultValue={game.description} id="description description-box" maxLength={'500'} className='form-control' cols="40" rows="3"></textarea>
                 </div>
                 <input className='btn btn-outline-success' type="submit" value="Edit Game" />
             </form>
