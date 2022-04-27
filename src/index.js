06f996e2bbf39d288bc541c7ad862ffe51754b04
Lucas <lucas2carlos@gmail.com>
2022-04-27 19:44:44 -0400

    fixing folder path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {React, StrictMode} from "react";
 import ReactDOM  from "react-dom";
-import App from "./App";
+import App from "./pages/App/App";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
