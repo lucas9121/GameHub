68ef63388f91811ae2598648d9e5176e3ccd4d65
Lucas <lucas2carlos@gmail.com>
2022-04-27 19:37:56 -0400

    fixing folder path

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const usersCtrl = require('../../controllers/api/user');
+const usersCtrl = require('../../controllers/api/users');
 const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
 
