a77fb1f227ddd35c2cf9c71106008ee042c3fc90
Lucas <lucas2carlos@gmail.com>
2022-04-27 19:47:32 -0400

    fixing folder path

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,12 +1,12 @@
 import './App.css'
 import {Routes, Route} from "react-router-dom"
 import { useState, useEffect } from "react"
-import Home from './pages/Home'
-import MyAccount from './pages/MyAccount'
-import Cart from './pages/Cart'
-import Show from './pages/Show'
-import Edit from './pages/Edit'
-import New from './pages/New'
+import Home from '../Home'
+import MyAccount from '../MyAccount'
+import Cart from '../Cart'
+import Show from '../Show'
+import Edit from '../Edit'
+import New from '../New'
 
 export default function App(){
     const [games, setGames] = useState([])
