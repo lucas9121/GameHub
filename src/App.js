f4af3d5c65f7999bdd16023642ff22a20ebff0be
Lucas <lucas2carlos@gmail.com>
2022-04-26 22:06:41 -0400

    fixing typo

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import New from './pages/New'
 
 export default function App(){
     const [games, setGames] = useState([])
-    const [refressh, setRefresh] = useState(false)
+    const [refresh, setRefresh] = useState(false)
 
     useEffect(() => {
         (async () => {
@@ -22,16 +22,16 @@ export default function App(){
                 console.log(e)
             }
         })()
-    }, [refressh])
+    }, [refresh])
 
     return(
         <main className='App'>
             <Routes>
                 <Route path='/' element={<Home games={games}/>} />
-                <Route path='/new' element={<New refressh={refressh} setRefresh={setRefresh}/>} />
+                <Route path='/new' element={<New refresh={refresh} setRefresh={setRefresh}/>} />
                 <Route path='/account' element={<MyAccount />} />
                 <Route path='/cart' element={<Cart />} />
-                <Route path='/:id/edit' element={<Edit refressh={refressh} setRefresh={setRefresh} />} />
+                <Route path='/:id/edit' element={<Edit refresh={refresh} setRefresh={setRefresh} />} />
                 <Route path='/:id' element={<Show />} />
             </Routes>
         </main>
