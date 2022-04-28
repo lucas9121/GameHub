cd0a640fb96d6a81c6739e780b446d7302181d9a
Lucas <lucas2carlos@gmail.com>
2022-04-27 22:59:42 -0400

    fixing class name

--- src/components/UserLogOut/UserLogOut.jsx
+++ src/components/UserLogOut/UserLogOut.jsx
@@ -5,9 +5,9 @@ export default function UserLogOut({ user, setUser }) {
       logOut();
       setUser(null);
     }
-    
+
     return (
-      <div className={styles.UserLogOut}>
+      <div className="UserLogOut">
         <div>{user.name}</div>
         <div className={styles.email}>{user.email}</div>
         <button className="btn-sm" onClick={handleLogOut}>LOG OUT</button>
