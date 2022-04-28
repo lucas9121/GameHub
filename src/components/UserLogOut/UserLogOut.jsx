941dc3ac73e5439922d8f5b76843d3fb54967aca
Lucas <lucas2carlos@gmail.com>
2022-04-27 23:04:00 -0400

    fixing typo

--- src/components/UserLogOut/UserLogOut.jsx
+++ src/components/UserLogOut/UserLogOut.jsx
@@ -9,7 +9,6 @@ export default function UserLogOut({ user, setUser }) {
     return (
       <div className="UserLogOut">
         <div>{user.name}</div>
-        <div className={styles.email}>{user.email}</div>
         <button className="btn-sm" onClick={handleLogOut}>LOG OUT</button>
       </div>
     );
