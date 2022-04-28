be26f703a2b0de630e9836f1fa8a7f472b6a2a53
Lucas <lucas2carlos@gmail.com>
2022-04-27 22:07:35 -0400

    adding sign up and log in

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,5 +1,16 @@
-export default function AuthPage () {
-    return (
-        <h1>Auth Page</h1>
-    )
+import { useState } from 'react';
+import LoginForm from '../../components/LoginForm/LoginForm';
+import SignUpForm from '../../components/SignUpForm/SignUpForm';
+
+export default function AuthPage({ setUser }) {
+  const [showLogin, setShowLogin] = useState(true);
+
+  return (
+    <main className="AuthPage">
+      <div>
+            <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}</h3>
+      </div>
+      {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
+    </main>
+  );
 }
