42c622257d41b5d586f21b70192e6c67dfa386d3
Lucas <lucas2carlos@gmail.com>
2022-04-29 02:14:05 -0400

    adding delete button

--- src/pages/MyAccount/MyAccount.js
+++ src/pages/MyAccount/MyAccount.js
@@ -1,4 +1,18 @@
-export default function MyAccount({user}) {
+import { useNavigate, useParams } from "react-router-dom"
+
+export default function MyAccount({user, refresh, setRefresh}) {
+    const {id} = useParams()
+    const navigate = useNavigate()
+
+    const handleDelete = async (event) => {
+        setRefresh(!refresh)
+        navigate('/')
+        try {
+            const res = await fetch(`http://localhost:3001/api/users/${id}`, {method: 'DELETE'})
+        } catch(e) {
+            console.log(e)
+        } 
+    }
     return(
         <div className='myAccount'>
             <h2>My Account</h2>
@@ -9,6 +23,7 @@ export default function MyAccount({user}) {
                 <p>{user.email} </p>
                 <h4>Account Type</h4>
                 <p style={{textTransform: 'capitalize'}}>{user.account}</p>
+                <button onClick={handleDelete}>Delete Account</button>
             </div>
         </div>
 )
