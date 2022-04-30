6474cfa44e3e6ad80dc21552ce1ef2161fac095b
Lucas <lucas2carlos@gmail.com>
2022-04-30 11:55:11 -0400

    fixing user delete

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -16,6 +16,7 @@ export default function App(){
     const [games, setGames] = useState([])
     const [refresh, setRefresh] = useState(false)
     const [user, setUser] = useState(getUser())
+    const [userDlt, setUserDlt] = useState(false)
 
     useEffect(() => {
         (async () => {
@@ -23,9 +24,12 @@ export default function App(){
                 const res = await fetch('http://localhost:3001/api/games')
                 const data = await res.json()
                 setGames(data)
-                if(!user){
-                    setUser()
+                if(userDlt){
+                    logOut()
+                    setUser(getUser())
+                    setUserDlt(false)
                 }
+                console.log(user)
             } catch (e) {
                 console.log(e)
             }
@@ -39,7 +43,7 @@ export default function App(){
             <Routes>
                 <Route path='/' element={<Home games={games} user={user}/>} />
                 <Route path='/new' element={<New refresh={refresh} setRefresh={setRefresh}/>} />
-                <Route path='/account/:id' element={<MyAccount user={user} setUser={setUser} refresh={refresh} setRefresh={setRefresh} />} />
+                <Route path='/account/:id' element={<MyAccount user={user} setUserDlt={setUserDlt} refresh={refresh} setRefresh={setRefresh} />} />
                 <Route path='/cart' element={<Cart />} />
                 <Route path='/:id/edit' element={<Edit refresh={refresh} setRefresh={setRefresh} />} />
                 <Route path='/:id' element={<Show user={user}/>} />
--- src/pages/MyAccount/MyAccount.js
+++ src/pages/MyAccount/MyAccount.js
@@ -1,18 +1,27 @@
 import { useNavigate, useParams } from "react-router-dom"
+// import { logOut } from "../../utilities/users-service"
 
-export default function MyAccount({user, refresh, setRefresh}) {
+export default function MyAccount({user, setUserDlt, refresh, setRefresh}) {
     const {id} = useParams()
     const navigate = useNavigate()
-    console.log(id)
+    // console.log(id)
 
     const handleDelete = async (event) => {
+        console.log(`User before try ${user}`)
         try {
+            console.log(`User is ${user}`)
             setRefresh(!refresh)
+            setUserDlt(true)
             navigate('/')
             const res = await fetch(`http://localhost:3001/api/users/delete/${id}`, {method: 'DELETE'})
+            console.log(`User is ${user}`)
         } catch(e) {
             console.log(e)
-        } 
+        } finally {
+            console.log(`user in finally is`)
+            console.log(user)
+        }
+        console.log(`User after catch ${user}`)
     }
     return(
         <div className='myAccount'>
