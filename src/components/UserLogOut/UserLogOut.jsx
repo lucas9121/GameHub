aa1be867a5e1cc1a44163fd9b8e1d0628dfd63c4
Lucas <lucas2carlos@gmail.com>
2022-04-30 10:28:34 -0400

    adding navigate

--- src/components/UserLogOut/UserLogOut.jsx
+++ src/components/UserLogOut/UserLogOut.jsx
@@ -1,9 +1,12 @@
 import { logOut } from '../../utilities/users-service';
+import {useNavigate} from "react-router-dom"
 
 export default function UserLogOut({ setUser }) {
+  const navigate = useNavigate()
     function handleLogOut() {
       logOut();
       setUser(null);
+      navigate('/')
     }
 
     return (
