f61cb8f180d837ef3f6f01cd8fe0d603903657ad
Lucas <lucas2carlos@gmail.com>
2022-04-30 12:50:28 -0400

    fixing user delete

--- src/pages/MyAccount/MyAccount.js
+++ src/pages/MyAccount/MyAccount.js
@@ -1,27 +1,22 @@
 import { useNavigate, useParams } from "react-router-dom"
-// import { logOut } from "../../utilities/users-service"
+import { logOut } from "../../utilities/users-service"
 
 export default function MyAccount({user, setUserDlt, refresh, setRefresh}) {
     const {id} = useParams()
     const navigate = useNavigate()
-    // console.log(id)
 
-    const handleDelete = async (event) => {
-        console.log(`User before try ${user}`)
+    const handleDelete = (event) => {
         try {
-            console.log(`User is ${user}`)
-            setRefresh(!refresh)
-            setUserDlt(true)
-            navigate('/')
-            const res = await fetch(`http://localhost:3001/api/users/delete/${id}`, {method: 'DELETE'})
-            console.log(`User is ${user}`)
+            fetch(`http://localhost:3001/api/users/delete/${id}`, {method: 'DELETE'})
         } catch(e) {
             console.log(e)
         } finally {
-            console.log(`user in finally is`)
-            console.log(user)
+            console.log('user deleted')
+            setRefresh(!refresh)
+            setUserDlt(true)
+            navigate('/')
+            logOut()
         }
-        console.log(`User after catch ${user}`)
     }
     return(
         <div className='myAccount'>
