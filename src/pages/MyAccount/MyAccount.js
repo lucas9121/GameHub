25653d31e6f95316eb9d2dade2e831bd0fb3637b
Lucas <lucas2carlos@gmail.com>
2022-05-02 19:43:38 -0400

    making hooks

--- src/pages/MyAccount/MyAccount.js
+++ src/pages/MyAccount/MyAccount.js
@@ -1,9 +1,11 @@
+import { useState, useRef } from "react"
 import { useNavigate, useParams } from "react-router-dom"
 import { logOut } from "../../utilities/users-service"
 
 export default function MyAccount({user, setUserDlt, refresh, setRefresh}) {
     const {id} = useParams()
     const navigate = useNavigate()
+    const [userCard, setUserCard] = useState([])
 
     const handleDelete = (event) => {
         // making this an async await makes the code terminate after the fetch request
@@ -29,6 +31,12 @@ export default function MyAccount({user, setUserDlt, refresh, setRefresh}) {
                 <p>{user.email} </p>
                 <h4>Account Type</h4>
                 <p style={{textTransform: 'capitalize'}}>{user.account}</p>
+                <h4>Card</h4>
+                {
+                    user.card.length === 0 ?
+                    <p>no card info</p> :
+                    <p></p>
+                }
                 <button>Add card info</button>
                 <button onClick={handleDelete}>Delete Account</button>
             </div>
