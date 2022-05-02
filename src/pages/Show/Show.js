4eeee2c856f2c3f136bf7ed8b971fe1e1d275002
Lucas <lucas2carlos@gmail.com>
2022-05-02 19:23:28 -0400

    fixing submit review

--- src/pages/Show/Show.js
+++ src/pages/Show/Show.js
@@ -126,6 +126,7 @@ export default function Show({user}) {
                     }
                     <div className="review-comments">
                         {
+                            reviews.length ?
                            reviews.map((review, idx) => {
                                 return(
                                     <div key={idx} className="form-group">
@@ -133,7 +134,8 @@ export default function Show({user}) {
                                         <p>{review.description} { user && user.account === "gamer" && user.name === review.name ? <button>Edit</button> : null } </p>
                                     </div>
                                 )
-                            })
+                            }) :
+                            ''
                         }
                     </div>
                 </div>
