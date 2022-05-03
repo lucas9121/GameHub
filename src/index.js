0f01084906b7651fad9419d39755d836732ed0c8
Lucas <lucas2carlos@gmail.com>
2022-05-03 18:35:22 -0400

    importing index css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {React, StrictMode} from "react";
 import ReactDOM  from "react-dom";
 import App from "./pages/App/App";
+import './index.css'
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
