37213f4d7a6fd3b34a41ca1382ed31768276d4f5
Lucas <lucas2carlos@gmail.com>
2022-05-04 02:00:34 -0400

    fixing display

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
 import LoginForm from '../../components/LoginForm/LoginForm';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
+import styles from './AuthPage.module.css'
 
 export default function AuthPage({ setUser }) {
   const [showLogin, setShowLogin] = useState(true);
 
 
   return (
-    <main className="AuthPage">
+    <main className={styles.AuthPage}>
       <LoginForm setUser={setUser} />
       <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}</h3>
     </main>
