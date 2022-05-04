a8fb97b4d5c931e9d8750d8d5e944436a8e55517
Lucas <lucas2carlos@gmail.com>
2022-05-03 22:03:41 -0400

    adding dropdown menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,21 +2,44 @@ import AuthPage from "../../pages/AuthPage/AuthPage"
 import UserLogOut from "../UserLogOut/UserLogOut"
 import { Link } from "react-router-dom"
 import styles from "./NavBar.module.css"
+import { useState } from "react"
 
 export default function NavBar({ user, setUser}) {
+    const [options, setOptions] = useState([])
+    const [toggle, setToggle] = useState(false)
+    const dropdown = () => {
+        setToggle(!toggle)
+        if(toggle){
+            if(user.account ===  "gamer"){
+                setOptions([{url: `/account/${user._id}`, name: 'My Account' }, {url: '/cart', name: 'Cart' } ]) 
+    
+            } else {
+                setOptions([{url: `/account/${user._id}`, name: 'My Account' }, {url: '/new', name: 'New Game' }])
+            }
+            <UserLogOut setUser={setUser}/>
+        } else {
+            setOptions([])
+        }
+        
+    }
     return(
         <nav className={styles.NavBar}>
             <Link to={'/'}><h1>GameHub</h1></Link>
             {
                 user  ?
                 <div>
-                    <p>Hello {user.name}</p>
-                    <Link to={`/account/${user._id}`} >My Account</Link>
-                    {
-                        user.account === "gamer" ?
-                        <Link to={'/cart'} >Cart</Link> :
-                        <Link to={'/new'}>New Game</Link>
-                    }
+                    <div>
+                        <p onClick={dropdown} onToggle={(evt) => {
+                            console.log('toggle')
+                        }} >Hello {user.name}</p>
+                            {
+                                options.map((option) => {
+                                    return(
+                                        <Link to={option.url}>{option.name}</Link>
+                                    )
+                                })
+                            }
+                    </div>
                     <UserLogOut setUser={setUser}/>
                 </div> :
                 <AuthPage user={user} setUser={setUser} />
