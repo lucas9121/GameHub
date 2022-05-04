6a8abeede7a14a695ee165c5c9e23bf0c1c98f64
Lucas <lucas2carlos@gmail.com>
2022-05-04 00:26:22 -0400

    fixing dropdown

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,26 +2,28 @@ import AuthPage from "../../pages/AuthPage/AuthPage"
 import UserLogOut from "../UserLogOut/UserLogOut"
 import { Link } from "react-router-dom"
 import styles from "./NavBar.module.css"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export default function NavBar({ user, setUser}) {
     const [options, setOptions] = useState([])
     const [toggle, setToggle] = useState(false)
+
     const dropdown = () => {
-        setToggle(!toggle)
+        setToggle(!toggle) 
+    }
+    
+    useEffect(() => {
         if(toggle){
             if(user.account ===  "gamer"){
-                setOptions([{url: `/account/${user._id}`, name: 'My Account' }, {url: '/cart', name: 'Cart' } ]) 
-    
+                setOptions([{url: `/account/${user._id}`, name: 'My Account' }, {url: '/cart', name: 'Cart' } ]);
             } else {
-                setOptions([{url: `/account/${user._id}`, name: 'My Account' }, {url: '/new', name: 'New Game' }])
+                setOptions([{url: `/account/${user._id}`, name: 'My Account' }, {url: '/new', name: 'New Game' }]);
             }
-            <UserLogOut setUser={setUser}/>
         } else {
             setOptions([])
-        }
-        
-    }
+        } 
+    }, [toggle])
+
     return(
         <nav className={styles.NavBar}>
             <Link to={'/'}><h1>GameHub</h1></Link>
@@ -31,13 +33,14 @@ export default function NavBar({ user, setUser}) {
                     <div className={styles.dropdown}>
                         {/* I had to add this div to center the dropdown item */}
                         <div><p></p></div>
-                        <button onClick={dropdown} onToggle={(evt) => {
-                            console.log('toggle')
-                        }} >Hello {user.name}</button>
+                        <button onClick={dropdown}>Hello {user.name}</button>
                             {
-                                options.map((option) => {
+                                options.map((option, idx) => {
                                     return(
-                                        <Link to={option.url}>{option.name}</Link>
+                                        idx === options.length - 1 ?
+                                        <Link to={option.url} style={{borderRadius: '0 0 10px 10px'}} onClick={dropdown} >{option.name}</Link>
+                                        :
+                                        <Link to={option.url} onClick={dropdown}>{option.name}</Link>
                                     )
                                 })
                             }
