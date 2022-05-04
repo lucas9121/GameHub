86efb4dc53682a68279a64232141935e81ca6413
Lucas <lucas2carlos@gmail.com>
2022-05-04 17:34:33 -0400

    cleaning code

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,13 +4,11 @@ import styles from './Home.module.css'
 export default function Home({games}) {
     return(
         <main className={styles.main}>
-            <h1>Home Page</h1>
             {
                 games.map((game) => {
                     return(
                         <div className={styles.sub} style={game.qty > 0 ? {border: 'solid green'} : {border: 'solid red'}}>
                             <Link style={{backgroundImage: `url(${game.img})`}} to={`/${game._id}`} alt={game.name} ></Link>
-                            {/* <img src={game.img} alt={game.name} width="400" height="400" /> */}
                             <div className={styles.banner}>
                                 <h2>{game.name} </h2>
                                 <div> 
