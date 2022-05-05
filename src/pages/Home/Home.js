0f91336bec45c2443086272cfd00986b17bb9942
Lucas <lucas2carlos@gmail.com>
2022-05-04 21:26:29 -0400

    adding accounts separation

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom"
 import styles from './Home.module.css'
 
-export default function Home({games}) {
+export default function Home({games, user}) {
     return(
+        // null error prevention
+        !user ?
         <main className={styles.main}>
             {
                 games.map((game) => {
@@ -16,10 +18,52 @@ export default function Home({games}) {
                                     {game.qty > 0 ? <p style={{color: 'green', fontSize: 'small'}}>Available</p> : <p style={{color: 'red', fontSize: 'small'}}>Sold Out</p> }
                                 </div>
                             </div>
-                        </div>
+                        </div> 
                     )
                 })
             }
-        </main>
+        </main> :
+        // if developer account is logged in
+        user.account === 'developer' ?
+        <main className={styles.main}>
+            {
+                games.map((game) => {
+                    return(
+                        // return only developer games
+                        user.name === game.dev ?
+                        <div className={styles.sub} style={game.qty > 0 ? {border: 'solid green'} : {border: 'solid red'}}>
+                            <Link style={{backgroundImage: `url(${game.img})`}} to={`/${game._id}`} alt={game.name} ></Link>
+                            <div className={styles.banner}>
+                                <h2>{game.name} </h2>
+                                <div> 
+                                    {game.price <= 0 ? <p>Free</p> : <p>${game.price}</p>}
+                                    {game.qty > 0 ? <p style={{color: 'green', fontSize: 'small'}}>Available</p> : <p style={{color: 'red', fontSize: 'small'}}>Sold Out</p> }
+                                </div>
+                            </div>
+                        </div> :
+                        null
+                    )
+                })
+            }
+        </main> :
+        // gamer and admin accounts
+        <main className={styles.main}>
+        {
+            games.map((game) => {
+                return(
+                    <div className={styles.sub} style={game.qty > 0 ? {border: 'solid green'} : {border: 'solid red'}}>
+                        <Link style={{backgroundImage: `url(${game.img})`}} to={`/${game._id}`} alt={game.name} ></Link>
+                        <div className={styles.banner}>
+                            <h2>{game.name} </h2>
+                            <div> 
+                                {game.price <= 0 ? <p>Free</p> : <p>${game.price}</p>}
+                                {game.qty > 0 ? <p style={{color: 'green', fontSize: 'small'}}>Available</p> : <p style={{color: 'red', fontSize: 'small'}}>Sold Out</p> }
+                            </div>
+                        </div>
+                    </div> 
+                )
+            })
+        }
+    </main>
     )
 }
