fc2d77d1249f4bf49c3431bc1eb09bb54c4ce417
Lucas <lucas2carlos@gmail.com>
2022-05-06 16:01:58 -0400

    edit functionality

--- src/pages/MyAccount/MyAccount.js
+++ src/pages/MyAccount/MyAccount.js
@@ -5,7 +5,11 @@ import { logOut } from "../../utilities/users-service"
 export default function MyAccount({user, setUserDlt, refresh, setRefresh}) {
     const {id} = useParams()
     const navigate = useNavigate()
-    const [userCard, setUserCard] = useState([])
+    const [editBtn, setEditBtn] = useState(false)
+    const [confirm, setConfirm] = useState(false)
+    const name = useRef(null)
+    const username = useRef(null)
+    const email = useRef(null)
 
     const handleDelete = (event) => {
         // making this an async await makes the code terminate after the fetch request
@@ -21,25 +25,62 @@ export default function MyAccount({user, setUserDlt, refresh, setRefresh}) {
             logOut()
         }
     }
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
+        try{
+            fetch(`http://localhost:3001/api/users/${id}`, {
+                method: 'PUT',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    name: name.current.value,
+                    username: username.current.value,
+                    email: email.current.value
+                })
+            })
+            console.log('edit made')
+            setRefresh(!refresh)
+        }catch(e){
+            console.log(e)
+        }
+    }
+
+
     return(
         <div className='myAccount'>
-            <h2>My Account</h2>
-            <div className="account-info">
-                <h4>Name</h4>
-                <p>{user.name}</p>
-                <h4>Email</h4>
-                <p>{user.email} </p>
-                <h4>Account Type</h4>
-                <p style={{textTransform: 'capitalize'}}>{user.account}</p>
-                {/* <h4>Card</h4>
-                {
-                    user.card.length === 0 ?
-                    <p>no card info</p> :
-                    <p></p>
-                }
-                <button>Add card info</button> */}
-                <button onClick={handleDelete}>Delete Account</button>
-            </div>
+            <h2>My Account</h2> 
+            {
+                editBtn ?
+                <div className="account-info" onSubmit={handleSubmit}>
+                    <form autoComplete="off">
+                    <label>Name</label>
+                    <input type="text" name="name" ref={name} defaultValue={user.name} />
+                    <label>Username</label>
+                    <input type="text" name="username" ref={username} defaultValue={user.username} />
+                    <label>Email</label>
+                    <input type="email" name="email" ref={email} defaultValue={user.email} />
+                    <label>Account Type</label>
+                    <select name="account" defaultValue={user.account} disabled>
+                        <option  value="gamer">Gamer</option>
+                        <option value='developer'>Developer</option>
+                        <option value='admin'>Admin</option>
+                    </select>
+                    <button type="submit">Edit Account</button>
+                    <button onClick={() => {setEditBtn(false)}} >Cancel</button>
+                    </form>
+                    <button onClick={handleDelete}>Delete Account</button>
+                </div> :
+                <div className="account-info">
+                    <h4>Name</h4>
+                    <p>{user.name}</p>
+                    <h4>Email</h4>
+                    <p>{user.email} </p>
+                    <h4>Account Type</h4>
+                    <p style={{textTransform: 'capitalize'}}>{user.account}</p>
+                    <button onClick={() => {setEditBtn(true)}}>Edit Account</button>
+                    <button onClick={handleDelete}>Delete Account</button>
+                </div>
+            }
         </div>
 )
 }
