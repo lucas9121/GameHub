d7a72542acddf8a08ba7daa865e16ac76df9254a
Lucas <lucas2carlos@gmail.com>
2022-05-07 06:55:45 -0400

    importing css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,17 @@
 import {BsLinkedin} from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs'
 import { Link } from "react-router-dom"
+import styles from './Footer.module.css'
 
 
 export default function Footer(){
     return (
-        <footer>
+        <footer className={styles.footer}>
             <nav aria-label='Footer Navigation' role="navigation">
                 <p>&copy; Lucas C.</p>
-                <ul>
-                    <li><Link to='https://github.com/lucas9121' target='_blank'><BsGithub /></Link></li>
-                    <li><Link to='https://www.linkedin.com/in/lucas-carlos-915b23127/' target='_blank'> <BsLinkedin /></Link></li>
+                <ul className={styles.ul}>
+                    <li><Link to='https://github.com/lucas9121' target='_blank'><BsGithub  style={{height: '3rem', width: '3rem'}}/></Link></li>
+                    <li><Link to='https://www.linkedin.com/in/lucas-carlos-915b23127/' target='_blank'> <BsLinkedin style={{height: '3rem', width: '3rem'}}/></Link></li>
                 </ul>
             </nav>
         </footer>
