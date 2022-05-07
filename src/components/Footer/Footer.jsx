4396bfbbe6df44fee9b65cb3d9ede26e4a0dc74e
Lucas <lucas2carlos@gmail.com>
2022-05-07 05:40:44 -0400

    adding icon

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import {BsLinkedin} from 'react-icons/bs'
+import { Link } from "react-router-dom"
+
+export default function Footer(){
+    return (
+        <main>
+            {BsLinkedin}
+        </main>
+    )
+}
+
