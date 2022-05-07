9e919ac0ee2309fd5e29960e64460a0a2dc12e06
Lucas <lucas2carlos@gmail.com>
2022-05-07 06:44:51 -0400

    adding footer links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,19 @@
 import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
 import { Link } from "react-router-dom"
+
 
 export default function Footer(){
     return (
-        <main>
-            {BsLinkedin}
-        </main>
+        <footer>
+            <nav aria-label='Footer Navigation' role="navigation">
+                <p>&copy; Lucas C.</p>
+                <ul>
+                    <li><Link to='https://github.com/lucas9121' target='_blank'><BsGithub /></Link></li>
+                    <li><Link to='https://www.linkedin.com/in/lucas-carlos-915b23127/' target='_blank'> <BsLinkedin /></Link></li>
+                </ul>
+            </nav>
+        </footer>
     )
 }
 
