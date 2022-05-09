74b390b1fa57a267963f36ce5083d1ed6ed69eb2
Lucas <lucas2carlos@gmail.com>
2022-05-08 21:50:42 -0400

    fixing display

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,15 +6,13 @@ import styles from './Footer.module.css'
 
 export default function Footer(){
     return (
-        <footer className={styles.footer}>
-            <nav aria-label='Footer Navigation' role="navigation">
-                <p>&copy; Lucas C.</p>
+            <nav className={styles.footer} aria-label='Footer Navigation' role="navigation">
                 <ul className={styles.ul}>
                     <li><Link to='https://github.com/lucas9121' target='_blank'><BsGithub  style={{height: '3rem', width: '3rem'}}/></Link></li>
+                    <li><p>&copy; Lucas C.</p></li>
                     <li><Link to='https://www.linkedin.com/in/lucas-carlos-915b23127/' target='_blank'> <BsLinkedin style={{height: '3rem', width: '3rem'}}/></Link></li>
                 </ul>
             </nav>
-        </footer>
     )
 }
 
