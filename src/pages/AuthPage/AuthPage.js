56ed12b3ee4f707194949611151456677b5478c1
Lucas <lucas2carlos@gmail.com>
2022-05-08 23:00:16 -0400

    adding sign in button

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -5,12 +5,20 @@ import styles from './AuthPage.module.css'
 
 export default function AuthPage({ setUser }) {
   const [showLogin, setShowLogin] = useState(true);
+  const [showSignin, setShowSignin] = useState(false)
 
 
   return (
     <main className={styles.AuthPage}>
-      <LoginForm setUser={setUser} />
-      <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}</h3>
+      <h3 onClick={() => setShowSignin(!showSignin)}>Sign in</h3>
+      {showSignin ?
+        <div>
+          <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}</h3>
+          {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
+        </div> :
+        null
+      }
+
     </main>
 
     // <main className="AuthPage">
