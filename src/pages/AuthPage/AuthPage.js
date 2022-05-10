2e8c52a7a96acc992ee5e58877f8bd6644f7ce3d
Lucas <lucas2carlos@gmail.com>
2022-05-10 18:21:46 -0400

    testing display setup

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -3,22 +3,19 @@ import LoginForm from '../../components/LoginForm/LoginForm';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import styles from './AuthPage.module.css'
 
-export default function AuthPage({ setUser }) {
+export default function AuthPage({ setUser, showSignin, setShowSignin }) {
   const [showLogin, setShowLogin] = useState(true);
-  const [showSignin, setShowSignin] = useState(false)
+  // const [showSignin, setShowSignin] = useState(false)
 
 
   return (
     <main className={styles.AuthPage}>
-      <h3 onClick={() => setShowSignin(!showSignin)}>Sign in</h3>
-      {showSignin ?
         <div>
           <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}</h3>
-          {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
-        </div> :
-        null
-      }
-
+          <div>
+            {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
+          </div>
+        </div> 
     </main>
 
     // <main className="AuthPage">
