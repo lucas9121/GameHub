7085c781928d18f55e528186fcdeb787e61b1f65
Lucas <lucas2carlos@gmail.com>
2022-05-11 17:24:46 -0400

    adding hook ternary

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,29 +1,22 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import LoginForm from '../../components/LoginForm/LoginForm';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import styles from './AuthPage.module.css'
 
-export default function AuthPage({ setUser, showSignin, setShowSignin }) {
+export default function AuthPage({ setUser }) {
   const [showLogin, setShowLogin] = useState(true);
-  // const [showSignin, setShowSignin] = useState(false)
 
 
   return (
     <main className={styles.AuthPage}>
         <div>
           <div>
-            <h3 onClick={() => setShowLogin(true)}> LOG IN</h3>
-            <h3 onClick={() => setShowLogin(false)}>SIGN UP</h3>
+            <h3 style={showLogin ? {border: 'solid rgb(0 123 255)'} : null} onClick={() => setShowLogin(true)}> LOG IN</h3>
+            <h3 style={!showLogin ? {border: 'solid rgb(0 123 255)'} : null} onClick={() => setShowLogin(false)}>SIGN UP</h3>
           </div>
+          {/* changes the div display */}
             {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
         </div> 
     </main>
-
-    // <main className="AuthPage">
-    //   <div>
-    //         <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}</h3>
-    //   </div>
-    //   {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
-    // </main>
-  );
+  )
 }
