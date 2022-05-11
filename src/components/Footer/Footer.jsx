e029a19fec748e1b94191e147952d3997850c5dd
Lucas <lucas2carlos@gmail.com>
2022-05-11 19:29:55 -0400

    fixing link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import {BsLinkedin} from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs'
-import { Link } from "react-router-dom"
 import styles from './Footer.module.css'
 
 
@@ -8,9 +7,9 @@ export default function Footer(){
     return (
             <nav className={styles.footer} aria-label='Footer Navigation' role="navigation">
                 <ul className={styles.ul}>
-                    <li><Link to='https://github.com/lucas9121' target='_blank'><BsGithub  style={{height: '3rem', width: '3rem'}}/></Link></li>
-                    <li><p>&copy; Lucas C.</p></li>
-                    <li><Link to='https://www.linkedin.com/in/lucas-carlos-915b23127/' target='_blank'> <BsLinkedin style={{height: '3rem', width: '3rem'}}/></Link></li>
+                    <li><a href='https://github.com/lucas9121' target='_blank'><BsGithub  style={{height: '3rem', width: '3rem'}}/></a></li>
+                    <li><p>&copy; Lucas C. 2022</p></li>
+                    <li><a href='https://www.linkedin.com/in/lucas-carlos-915b23127/' target='_blank'> <BsLinkedin style={{height: '3rem', width: '3rem'}}/></a></li>
                 </ul>
             </nav>
     )
