1777961538ebc5e75114a275e37f08e54b9403df
Lucas <lucas2carlos@gmail.com>
2022-05-15 18:02:36 -0400

    adding css

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -47,32 +47,48 @@ export default class SignUpForm extends Component {
       const disable = this.state.password !== this.state.confirm;
       return (
         <div>
-          <div className="form-container">
             <form autoComplete="off" onSubmit={this.handleSubmit}>
-              <label>First Name</label>
-              <input type="text" name="fName" value={this.state.fName} onChange={this.handleChange} required />
-              <label>Last Name</label>
-              <input type="text" name="lName" value={this.state.lName} onChange={this.handleChange} required />
-              <label>Username</label>
-              <input type="text" name="username" value={this.state.username} onChange={this.handleChange} required />
-              <label>Email</label>
-              <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
-              <label>Password</label>
-              <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
-              <label>Confirm</label>
-              <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
-              <label>Account Type</label>
-              <select name="account" onChange={(evt) => {
-                // options are elements in the select array. account is equal to the value of the clicked option
-                  this.setState({...this.state, account: evt.target[evt.target.selectedIndex].value} )
-              }}>
-                  <option  value="gamer">Gamer</option>
-                  <option value='developer'>Developer</option>
-                  <option value='admin'>Admin</option>
-              </select>
-              <button type="submit" disabled={disable}>SIGN UP</button>
+              <div className="first-row">
+                <div>
+                  <label>First Name</label>
+                  <input type="text" name="fName" value={this.state.fName} onChange={this.handleChange} required />
+                </div>
+                <div>
+                  <label>Last Name</label>
+                  <input type="text" name="lName" value={this.state.lName} onChange={this.handleChange} required />
+                </div>
+                <div>
+                  <label>Username</label>
+                  <input type="text" name="username" value={this.state.username} onChange={this.handleChange} required />
+                </div>
+              </div>
+              <div className="second-row">
+                <div>
+                  <label>Email</label>
+                  <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
+                </div>
+                <div>
+                  <label>Password</label>
+                  <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
+                </div>
+                <div>
+                  <label>Confirm</label>
+                  <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
+                </div>
+              </div>
+              <div className="third-row">
+                <label>Account Type</label>
+                <select name="account" onChange={(evt) => {
+                  // options are elements in the select array. account is equal to the value of the clicked option
+                    this.setState({...this.state, account: evt.target[evt.target.selectedIndex].value} )
+                }}>
+                    <option  value="gamer">Gamer</option>
+                    <option value='developer'>Developer</option>
+                    <option value='admin'>Admin</option>
+                </select>
+                <button type="submit" disabled={disable}>SIGN UP</button>
+              </div>
             </form>
-          </div>
           <p className="error-message">&nbsp;{this.state.error}</p>
         </div>
       );
