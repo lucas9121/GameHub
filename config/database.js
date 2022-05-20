f81df7026889d5d05272a5f3c345c64f64d6d5c4
Lucas <lucas2carlos@gmail.com>
2022-05-20 18:29:18 -0400

    fixing typo

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 
 
-db.on('conneected', () => {
+db.on('connected', () => {
     console.log(`Connected to ${db.name} at ${db.host}:${db.port}`)
 })
 
