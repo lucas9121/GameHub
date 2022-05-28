55b44c2fe19f206bb85673e46e676e848ad6b689
Lucas <lucas2carlos@gmail.com>
2022-05-28 13:24:08 -0400

    adding get all function to admin account hompage

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,45 @@
 import { Link } from "react-router-dom"
 import styles from './Home.module.css'
+import { useEffect, useState } from "react";
+import * as UsersAPI from '../../utilities/users-api'
+import { getAllUsers } from "../../utilities/users-service";
 
 export default function Home({games, user}) {
     const newArr = [...games];
-    console.log('GAMES!!!!!!!!!!!!!!!!!')
-    console.log(games)
-    console.log('NEW ARRAY!!!!!!!!!!!!!!!!')
-    console.log(newArr)
+    const [allUsers, setAllUsers] = useState([])
+
+    // useEffect(() => {
+    //     (async () => {
+    //         try{
+    //             const res = await UsersAPI.getAll()
+    //             // setAllUsers(res)
+    //             sessionStorage.setItem('sessionToken', res)
+    //             setAllUsers(getAllUsers())
+    //         } catch(e){
+    //             console.log(e)
+    //         }
+    //     })()
+    // }, [])
+
+    const handleClick = async() => {
+        try{
+            const res = await UsersAPI.getAll()
+            // setAllUsers(res)
+            sessionStorage.setItem('sessionToken', res)
+            setAllUsers(getAllUsers())
+            if(res.status === 200){
+                console.log(allUsers)
+            }
+        } catch(e){
+            console.log(e)
+        } finally {
+            console.log('function is working')
+        }
+    }
+
+    console.log(allUsers)
+
+
     return(
         // null error prevention
         //if nobody is logged in
@@ -56,32 +89,37 @@ export default function Home({games, user}) {
         </main> :
         user.account === 'admin' ?
         // admin accounts
-        <main className={styles.main}>
-        {
-            newArr.sort((a, b) => {
-                // if(a.approved === 'review'){
-                //     return 1
-                // }
-                if(b.approved === 'review'){
-                    return -1
-                }
-                return 0
-            }).reverse().map((game) => {
-                return(
-                    <div className={styles.sub} style={game.approved === 'review' ? {border: 'solid gold'} : game.approved === 'yes' && game.qty > 0 ? {border: 'solid green'} : {border: 'solid red'}}>
-                        <Link style={{backgroundImage: `url(${game.img})`}} to={`/${game._id}`} alt={game.name} ></Link>
-                        <div className={styles.banner}>
-                            <h2>{game.name} </h2>
-                            <div> 
-                                {game.price <= 0 ? <p>Free</p> : <p>${game.price}</p>}
-                                {game.qty > 0 ? <p style={{color: 'green', fontSize: 'small'}}>Available</p> : <p style={{color: 'red', fontSize: 'small'}}>Sold Out</p> }
+        <main>
+            <button className="btn main-btn" onClick={handleClick}>Users</button>
+            <div className={styles.main}>
+            {
+                newArr.sort((a, b) => {
+                    // if(a.approved === 'review'){
+                    //     return 1
+                    // }
+                    if(b.approved === 'review'){
+                        return -1
+                    }
+                    return 0
+                }).reverse().map((game) => {
+                    return(
+                        <div className={styles.sub} style={game.approved === 'review' ? {border: 'solid gold'} : game.approved === 'yes' && game.qty > 0 ? {border: 'solid green'} : {border: 'solid red'}}>
+                            <Link style={{backgroundImage: `url(${game.img})`}} to={`/${game._id}`} alt={game.name} ></Link>
+                            <div className={styles.banner}>
+                                <h2>{game.name} </h2>
+                                <div> 
+                                    {game.price <= 0 ? <p>Free</p> : <p>${game.price}</p>}
+                                    {game.qty > 0 ? <p style={{color: 'green', fontSize: 'small'}}>Available</p> : <p style={{color: 'red', fontSize: 'small'}}>Sold Out</p> }
+                                </div>
                             </div>
-                        </div>
-                    </div> 
-                )
-            })
-        }
-    </main> :
+                        </div> 
+                    )
+                })
+            }
+        </div> 
+
+        </main>
+    :
     //gamer account
     <main className={styles.main}>
     {
