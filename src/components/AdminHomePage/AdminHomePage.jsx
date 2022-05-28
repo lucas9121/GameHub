dd2507d8591835fb0ecb36aa988884fa35be2c09
Lucas <lucas2carlos@gmail.com>
2022-05-28 13:30:04 -0400

    adding admin home page

---
+++ src/components/AdminHomePage/AdminHomePage.jsx
@@ -0,0 +1,38 @@
+
+
+export default function AdminHomePage({games, handleClick}) {
+    const newArr = [...games];
+
+    return(
+        <main>
+            <button className="btn main-btn" onClick={handleClick}>Users</button>
+            <div className={styles.main}>
+                {
+                    newArr.sort((a, b) => {
+                        // if(a.approved === 'review'){
+                        //     return 1
+                        // }
+                        if(b.approved === 'review'){
+                            return -1
+                        }
+                        return 0
+                    }).reverse().map((game) => {
+                        return(
+                            <div className={styles.sub} style={game.approved === 'review' ? {border: 'solid gold'} : game.approved === 'yes' && game.qty > 0 ? {border: 'solid green'} : {border: 'solid red'}}>
+                                <Link style={{backgroundImage: `url(${game.img})`}} to={`/${game._id}`} alt={game.name} ></Link>
+                                <div className={styles.banner}>
+                                    <h2>{game.name} </h2>
+                                    <div> 
+                                        {game.price <= 0 ? <p>Free</p> : <p>${game.price}</p>}
+                                        {game.qty > 0 ? <p style={{color: 'green', fontSize: 'small'}}>Available</p> : <p style={{color: 'red', fontSize: 'small'}}>Sold Out</p> }
+                                    </div>
+                                </div>
+                            </div> 
+                        )
+                    })
+                }
+            </div> 
+        </main>
+    )
+
+}
