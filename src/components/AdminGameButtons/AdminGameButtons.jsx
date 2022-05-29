148a8eb6e120662d19dc4281c9e8bf82c8844ed7
Lucas <lucas2carlos@gmail.com>
2022-05-29 01:27:48 -0400

    adding css

--- src/components/AdminGameButtons/AdminGameButtons.jsx
+++ src/components/AdminGameButtons/AdminGameButtons.jsx
@@ -1,41 +1,44 @@
+import styles from './AdminGameButtons.module.css'
 
 export default function AdminGameButtons({game, noBtn, setNoBtn, noReason, handleApproved}){
     return(
-        <div>
+        <div className={styles.Admin}>
             {
             // if admin has already rejected the game before
                 game.reason ?
                 <>
-                    <h4>Previous comment</h4>
-                    <p>{game.reason}</p>
+                    <h4>Previous Comment:</h4>
+                    <div>
+                        <p>{game.reason}</p>
+                    </div>
                     <>
-                        <h5 style={{marginBottom: '10px'}}>Approved?</h5>
-                        <div style={{display: 'flex', marginBottom: '10px', gap: '5px'}}>
+                        <h5>Approved?</h5>
+                        <btns className={styles.Buttons}>
                             <button className="btn yes-btn" onClick={(evt) => {handleApproved(evt, 'yes')}}>Yes</button>
                             <button className="btn no-btn" onClick={() => {setNoBtn(true)}}>No</button>
-                        </div> 
+                        </btns> 
                     </>
                 </> :
                 // if game hasn't been rejected before
                 <>
                     <h5 style={{marginBottom: '10px'}}>Approved?</h5>
-                    <div style={{display: 'flex', marginBottom: '10px', gap: '5px'}}>
+                    <btns className={styles.Buttons}>
                         <button className="btn yes-btn" onClick={(evt) => {handleApproved(evt, 'yes')}}>Yes</button>
                         <button className="btn no-btn" onClick={(evt) => {setNoBtn(true)}}>No</button>
-                    </div> 
+                    </btns> 
                 </>
             }
             {
                 // if admin didn't approve the game show the text box to give reasons why
-                noBtn ?
+                noBtn &&
                 <form onSubmit={(evt) => {handleApproved(evt, 'no')}}>
                     <label>Explain what needs to be changed</label>
                     <textarea cols="40" rows="3" ref={noReason} required></textarea>
-                    <div>
+                    <btns className={styles.Buttons}>
                         <button type="submit" className="btn yes-btn">Submit</button>
                         <button className="btn no-btn" onClick={() => {setNoBtn(false)}}>Cancel</button>
-                    </div>
-                </form> : null
+                    </btns>
+                </form> 
             }
         </div>
     )
