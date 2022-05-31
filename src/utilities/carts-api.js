c2e01c8b8597feea241e7278580e0f75e91e9ad3
Lucas <lucas2carlos@gmail.com>
2022-05-31 18:27:39 -0400

    adding updateCart function and updating addToCart function

--- src/utilities/carts-api.js
+++ src/utilities/carts-api.js
@@ -20,25 +20,53 @@ export async function compareCarts(userId, strgCart){
     const dataCart = await sendRequest(`${BASE_URL}?user=${userId}`)
     for(let i = 0; i <dataCart.length; i++){
         const foundCart = strgCart.find((obj) => obj.game._id === dataCart[i].game._id)
-        console.log(foundCart)
         if(foundCart){
             dataCart[i].quantity += strgCart.quantity
+            try{
+                await sendRequest(BASE_URL, 'PUT', dataCart[i])
+            }catch(err){
+                console.log(`${err} in utitilies`)
+            }
         }
-        // return updateCart(dataCart)
     }
+    return getCart(userId)
 }
 
 
 // Creates cart schema (only if there's a user) and adds it to storage
 export function addToCart(payload){
     const cart = JSON.parse(sessionStorage.getItem('cart'))
+    let foundCart = cart.find((obj) => obj.game._id === payload.game._id )
     const newCart = [...cart, payload]
+    // if game is already in a cart schema
+    if(foundCart){
+        foundCart.quantity += 1
+        const index = cart.indexOf(foundCart)
+        cart.splice(index, 1, foundCart)
+        // if user is logged in
+        if(payload.user){
+            return updateCart(foundCart)
+        } else {
+            return sessionStorage.setItem('cart', JSON.stringify(cart))
+        }
+    }
+    // if game is not in a schema
+    //if user is logged in
     if(payload.user){
-        sessionStorage.setItem('cart', JSON.stringify(newCart))
-        return sendRequest(BASE_URL, 'POST', payload)
+        return updateCart(newCart)
     } else {
-        sessionStorage.setItem('cart', JSON.stringify(newCart))
-        console.log(new Blob(Object.values(sessionStorage)).size)
+        return sessionStorage.setItem('cart', JSON.stringify(newCart))
+    }
+}
+
+export async function updateCart(payload){
+    if(payload.user){
+        try{
+            await sendRequest(BASE_URL, 'PUT', payload)
+            return getCart(payload.user)
+        }catch(err){
+            console.log(`${err} in utitilies`)
+        }
     }
 }
 
