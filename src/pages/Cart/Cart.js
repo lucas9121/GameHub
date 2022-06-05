7511d6b06c2c6ad91d56fe48849aebde077510e6
Lucas <lucas2carlos@gmail.com>
2022-06-05 16:46:26 -0400

    adding cart info and styles

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -9,17 +9,24 @@ export default function Cart({user, cart, setCart, refresh, setRefresh, setSignI
                 cart.length === 0 ? <h2 className='text-center text-light'>Cart is Empty</h2> :
                 cart.map((cartItem) => {
                     return(
-                        <div className='cart-info'>
-                            <img src={cartItem.games[0].img} alt={cartItem.games[0].img} />
-                            <h2>{cartItem.games[0].name}</h2>
-                            <div className='cart-choice'>
-                                {cartItem.games[0].price <= 0 ? <p>Free</p> : <p>${cartItem.games[0].price * cartItem.quantity} </p>}
-                                <form className='buy-form'>
-                                    Qty: <input name="buyNumber" className='qty-input' type="number" defaultValue={cartItem.quantity}/>
-                                    {cartItem.games[0].qty > 0 ? <p className="stock">In Stock</p> : <p className="stock" style={{color: 'red'}}>Out of Stock</p> }
-                                    <input type="submit" className='btn yes-btn' value='Buy' />
-                                </form>
-                                <button type="submit" className='btn no-btn' value='Remove'>Remove</button>
+                        <div className={styles.Game} style={cartItem.quantity <= cartItem.games[0].qty ? {border: 'solid green'}: {border: 'solid red'}}>
+                            {/* <img src={cartItem.games[0].img} alt={cartItem.games[0].img} /> */}
+                            <div style={{backgroundImage: `url(${cartItem.games[0].img})`}}></div>
+                            <div>
+                                <h2>{cartItem.games[0].name}</h2>
+                                <div className={styles.GameInfo}>
+                                    {cartItem.games[0].price <= 0 ? <p>Free</p> : <p>${cartItem.games[0].price * cartItem.quantity} </p>}
+                                    <form className={styles.Form}>
+                                        <div>
+                                            <label>Quantity:</label>
+                                            <input name="buyNumber" className={styles.input} type="number" defaultValue={cartItem.quantity}/>
+                                        </div>
+                                        {cartItem.games[0].qty > 0 ? <p className={styles.stock}>In Stock</p> : <p className={styles.stock} style={{color: 'red'}}>Out of Stock</p> }
+                                        <input type="submit" className='btn yes-btn' value='Buy' />
+                                        <button className='btn no-btn'>Remove</button>
+                                    </form>
+                                </div>
+
                             </div>
                         </div>
                     )
