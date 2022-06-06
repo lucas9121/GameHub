85a46d7afe82ea3ece301752bc7c817725c3b1f1
Lucas <lucas2carlos@gmail.com>
2022-06-05 20:48:43 -0400

    fixing styling

--- src/components/DevHomePage/DevHomePage.jsx
+++ src/components/DevHomePage/DevHomePage.jsx
@@ -4,25 +4,25 @@ import styles from "./DevHomePage.module.css"
 export default function DevHomePage({games}){
     return(
         <div className={styles.main}>
-        {
-            games.map((game) => {
-                return(
-                    // return only developer games
-                    user.name === game.dev ?
-                    <div className={styles.sub} style={game.approved === 'review' ? {border: 'solid gold'} : game.approved === 'yes' && game.qty > 0 ? {border: 'solid green'} : {border: 'solid red'}}>
-                        <Link style={{backgroundImage: `url(${game.img})`}} to={`/${game._id}`} alt={game.name} ></Link>
-                        <div className={styles.banner}>
-                            <h2>{game.name} </h2>
-                            <div> 
-                                {game.price <= 0 ? <p>Free</p> : <p>${game.price}</p>}
-                                {game.qty > 0 ? <p style={{color: 'green', fontSize: 'small'}}>Available</p> : <p style={{color: 'red', fontSize: 'small'}}>Sold Out</p> }
+            {
+                games.map((game) => {
+                    return(
+                        // return only developer games
+                        user.name === game.dev ?
+                        <div className={styles.sub} style={game.approved === 'review' ? {border: 'solid gold'} : game.approved === 'yes' && game.qty > 0 ? {border: 'solid green'} : {border: 'solid red'}}>
+                            <Link style={{backgroundImage: `url(${game.img})`}} to={`/${game._id}`} alt={game.name} ></Link>
+                            <div className={styles.banner}>
+                                <h2>{game.name} </h2>
+                                <div> 
+                                    {game.price <= 0 ? <p>Free</p> : <p>${game.price}</p>}
+                                    {game.qty > 0 ? <p style={{color: 'green', fontSize: 'small'}}>Available</p> : <p style={{color: 'red', fontSize: 'small'}}>Sold Out</p> }
+                                </div>
                             </div>
-                        </div>
-                    </div> :
-                    null
-                )
-            })
-        }
-    </div> 
+                        </div> :
+                        null
+                    )
+                })
+            }
+        </div> 
     )
 }
