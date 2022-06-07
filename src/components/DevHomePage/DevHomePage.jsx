2e57cbec853ef43d8ad744d0825603a45bffc807
Lucas <lucas2carlos@gmail.com>
2022-06-07 19:58:23 -0400

    removing ternary

--- src/components/DevHomePage/DevHomePage.jsx
+++ src/components/DevHomePage/DevHomePage.jsx
@@ -7,8 +7,6 @@ export default function DevHomePage({games, user}){
             {
                 games.map((game) => {
                     return(
-                        // return only developer games
-                        user.name === game.dev ?
                         <div className={styles.sub} style={game.approved === 'review' ? {border: 'solid gold'} : game.approved === 'yes' && game.qty > 0 ? {border: 'solid green'} : {border: 'solid red'}}>
                             <Link style={{backgroundImage: `url(${game.img})`}} to={`/${game._id}`} alt={game.name} ></Link>
                             <div className={styles.banner}>
@@ -18,8 +16,7 @@ export default function DevHomePage({games, user}){
                                     {game.qty > 0 ? <p style={{color: 'green', fontSize: 'small'}}>Available</p> : <p style={{color: 'red', fontSize: 'small'}}>Sold Out</p> }
                                 </div>
                             </div>
-                        </div> :
-                        null
+                        </div>
                     )
                 })
             }
--- src/components/InitialHomePage/InitialHomePage.jsx
+++ src/components/InitialHomePage/InitialHomePage.jsx
@@ -7,8 +7,6 @@ export default function InitialHomePage({games}){
             {
                 games.map((game) => {
                     return(
-                        // if admin approved the game display it
-                        game.approved === 'yes' ?
                         <div className={styles.sub} style={game.qty > 0 ? {border: 'solid green'} : {border: 'solid red'}}>
                             <Link style={{backgroundImage: `url(${game.img})`}} to={`/${game._id}`} alt={game.name} ></Link>
                             <div className={styles.banner}>
@@ -18,7 +16,7 @@ export default function InitialHomePage({games}){
                                     {game.qty > 0 ? <p style={{color: 'green', fontSize: 'small'}}>Available</p> : <p style={{color: 'red', fontSize: 'small'}}>Sold Out</p> }
                                 </div>
                             </div>
-                        </div> : null
+                        </div>
                     )
                 })
             }
