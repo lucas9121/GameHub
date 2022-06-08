0790d945155c6593909352b6ccaced982bc45c3a
Lucas <lucas2carlos@gmail.com>
2022-06-07 20:20:32 -0400

    updating tag

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,13 +5,13 @@ import styles from './Footer.module.css'
 
 export default function Footer(){
     return (
-            <nav className={styles.footer} aria-label='Footer Navigation' role="navigation">
-                <ul className={styles.ul}>
+            <footer className={styles.footer} aria-label='Footer Navigation' role="navigation">
+                <ul className={styles.unordered}>
                     <li><a href='https://github.com/lucas9121' target='_blank'><BsGithub  style={{height: '2rem', width: '3rem'}}/></a></li>
                     <li><p>&copy; Lucas C. 2022</p></li>
                     <li><a href='https://www.linkedin.com/in/lucas-carlos-915b23127/' target='_blank'> <BsLinkedin style={{height: '2rem', width: '3rem'}}/></a></li>
                 </ul>
-            </nav>
+            </footer>
     )
 }
 
