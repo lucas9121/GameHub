b0fbdfd0577e184c6c816c5d08a3daeaf607a2a5
Lucas <lucas2carlos@gmail.com>
2022-09-09 20:21:22 -0400

    importing admin dashboard

--- src/pages/Data/Data.js
+++ src/pages/Data/Data.js
@@ -1,8 +1,15 @@
 import styles from './Data.module.css'
+import AdminDashboard from '../../components/AdminDashboard/AdminDashboard'
 export default function DataPage({user, games}){
-    return(
+    return(  
         <div className={styles.Data}>
-            <h1>Data Page</h1>
+            {
+                // if admin account is logged in
+                user.account === 'admin' ?
+                <AdminDashboard user={user} games={games} /> :
+                // if developer account is logged in
+                <h1>Data Page</h1>
+            }
         </div>
     )
 }
