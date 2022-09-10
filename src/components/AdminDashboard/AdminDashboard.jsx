5ecf431ad090f3601629fde4022a2d4504909864
Lucas <lucas2carlos@gmail.com>
2022-09-09 20:21:49 -0400

    building dashboard boilerplate

---
+++ src/components/AdminDashboard/AdminDashboard.jsx
@@ -0,0 +1,9 @@
+import styles from './AdminDashboard.module.css'
+
+export default function AdminDashboard({user, games}){
+    return(
+        <div>
+            <h1>Dashboard</h1>
+        </div>
+    )
+}
