c3c10f3624fe88c20994b9ee89ef075d7652247f
Lucas <lucas2carlos@gmail.com>
2022-09-20 17:47:17 -0400

    removing fetch call

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,27 +1,10 @@
-import { useState } from "react";
-import * as UsersAPI from '../../utilities/users-api'
-import { getAllUsers } from "../../utilities/users-service";
 import AdminHomePage from "../../components/AdminHomePage/AdminHomePage";
 import InitialHomePage from "../../components/InitialHomePage/InitialHomePage";
 import DevHomePage from "../../components/DevHomePage/DevHomePage";
 import GamerHomePage from "../../components/GamerHomePage/GamerHomePage";
 
 export default function Home({games, user}) {
-    const [allUsers, setAllUsers] = useState([])
 
-    //Finds every user. Only admin can use this function
-    const handleClick = async() => {
-        try{
-            const res = await UsersAPI.getAll()
-            // setAllUsers(res)
-            sessionStorage.setItem('sessionToken', res)
-            setAllUsers(getAllUsers())
-        } catch(e){
-            console.log(e)
-        } finally {
-            console.log('function is working')
-        }
-    }
 
 
     return(
@@ -34,7 +17,7 @@ export default function Home({games, user}) {
             <DevHomePage games={games.filter((game) => game.dev === user._id)} user={user}/> :
         // if admin account is logged in
         user.account === 'admin' ?
-            <AdminHomePage games={games} handleClick={handleClick} /> :
+            <AdminHomePage games={games} /> :
         // if gamer account is logged in
         <GamerHomePage games={games.filter((game) => game.approved === 'yes')}/>
     )
