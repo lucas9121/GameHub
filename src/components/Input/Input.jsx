0b9baaa4b9f4272bec14179d8d90d574ed81025f
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-11-20 01:14:17 -0500

    adding css

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,6 +1,7 @@
+import styles from './Input.module.css'
 
-export default function Input({type='text', inlineStyle={}, name='', value='', defaultValue='', id='', className='', ref='', isRequired=false, isDisabled=false, onChange=() => {} }) {
+export default function Input({type='text', inlineStyle={}, name='', value='', defaultValue='', id='', ref='', isRequired=false, isDisabled=false, onChange=() => {} }) {
     return(
-        <input style={inlineStyle} type={type} name={name} value={value} defaultValue={defaultValue} id={id} className={className} ref={ref} onChange={onChange} required={isRequired} disabled={isDisabled}/>
+        <input style={inlineStyle} type={type} name={name} value={value} defaultValue={defaultValue} id={id} className={styles.input} ref={ref} onChange={onChange} required={isRequired} disabled={isDisabled}/>
     )
 }
